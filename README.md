ef8e98f055e86e4c09ee67b7ef02fcca110b8299
Majd Lefi <majd.lefi@esprit.tn>
2021-02-19 09:07:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a movie app with router . where you can search the movie you want either by name or by rate . you can add a movie to the list and in each movie you can go for his description and watch his trailer.
+
+here's the movieList:
+![](movie%20app/Capture.PNG)
+You can add a movie
+![](movie%20app/addmovie.PNG)
+![](movie%20app/movieadded.PNG)
+and see his description and his trailer
+![](movie%20app/moviedescriptionwithtrailer.PNG)
+
+You can either search by name of movie or by trailer
+![](movie%20app/seachbyname.PNG)
+![](movie%20app/searchbyrate.PNG)
---
+++ src/components/ListMovies.js
@@ -0,0 +1,28 @@
+export const ListsMovies =[{
+    id:Math.random(),
+    title:"Tyler Rake",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://freakingeek.com/wp-content/uploads/2020/04/TylerRake-Banniere.jpg",
+    rate:3,
+    trailler:"https://www.youtube.com/watch?v=V95ZyU3QMic"
+
+},
+{
+    id:Math.random(),
+    title:"Catch me if you can",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://madisonsmovies.files.wordpress.com/2016/01/catch-me-if-you-can-movie-poster-2002-1020233910.jpg",
+    rate:5,
+    trailler:"https://www.youtube.com/watch?v=71rDQ7z4eFg"
+
+},
+{
+    id:Math.random(),
+    title:"12 years a slave",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://reelthinking.files.wordpress.com/2014/10/12_years_a_slave_posterart.jpg",
+    rate:4,
+    trailler:"https://www.youtube.com/watch?v=gRZe0nyex-U"
+
+}
+]
